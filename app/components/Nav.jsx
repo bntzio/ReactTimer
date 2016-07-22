d64f032ab5fab9d4c7baf365d53d49072e20ac8b
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-07-22 18:34:07 -0500

    Create & style Nav component

---
+++ app/components/Nav.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            ReactTimer
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="https://bntz.io" target="_blank">bntz.io</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Nav;
